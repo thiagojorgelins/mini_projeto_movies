1e07bec8ac8c9cb1b7e767c83103ad8bed656606
Thiago Lins <thiago.jorge2001@gmail.com>
2026-09-14 10:26:16 -0300

    feat: adiciona restrição de aluguel de filme, impedindo de alugar o mesmo filme mais de uma vez

--- lib/src/features/movies/ui/movie_page.dart
+++ lib/src/features/movies/ui/movie_page.dart
@@ -20,12 +20,34 @@ class _MoviePageState extends State<MoviePage> {
   final _loginController = getIt<LoginController>();
 
   Future<void> rentMovie() async {
-    final res = await _moviesController.rentalMovies(
-      _loginController.userId.value,
-      _moviesController.movieById.value.id,
-    );
-    if (res) {
-      context.go("/movies");
+    var userMovies = _moviesController.userMovies;
+    bool userMovie = userMovies.contains(_moviesController.movieById.value);
+    if (userMovie) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Não é possível alugar este filme"),
+            content: const Text("Filme já foi alugado"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      final res = await _moviesController.rentalMovies(
+        _loginController.userId.value,
+        _moviesController.movieById.value.id,
+      );
+      if (res) {
+        context.go("/movies");
+      }
     }
   }
 
